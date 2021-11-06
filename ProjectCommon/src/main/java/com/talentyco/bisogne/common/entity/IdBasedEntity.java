f589e611212cde4f7aa50cda82d2a882814dd4de
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-11-05 21:54:18 -0300

    Project Config

--- ProjectCommon/src/main/java/com/talentyco/bisogne/common/entity/IdBasedEntity.java
+++ ProjectCommon/src/main/java/com/talentyco/bisogne/common/entity/IdBasedEntity.java
@@ -19,8 +19,7 @@ public abstract class IdBasedEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
- 
+    
     public Integer getId() {
         return id;
     }
