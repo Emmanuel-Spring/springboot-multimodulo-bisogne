ac3455ae3bc9b6e168f7d471a1c9c844bfbf68cf
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-11-08 18:02:14 -0300

    Project Config

---
+++ ProjectWebParent/src/main/java/com/talentyco/springboot/ProjectWebParentApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebParentApplication.class, args);
+	}
+
+}
