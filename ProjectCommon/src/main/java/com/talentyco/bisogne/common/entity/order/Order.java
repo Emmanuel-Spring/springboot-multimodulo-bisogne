83c53b07ad1044def8d257957786dcbc8ce1617f
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2021-12-08 23:13:35 -0300

    Config Template

--- ProjectCommon/src/main/java/com/talentyco/bisogne/common/entity/order/Order.java
+++ ProjectCommon/src/main/java/com/talentyco/bisogne/common/entity/order/Order.java
@@ -1,8 +1,174 @@
 package com.talentyco.bisogne.common.entity.order;
 
 
+import com.talentyco.bisogne.common.entity.AbstractAddress;
+import com.talentyco.bisogne.common.entity.Customer;
 
-public class Order {
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "orders")
+public class Order extends AbstractAddress {
+
+    @Column(nullable = false, length = 45)
+    private String Country;
+
+    private Date orderTime;
+
+    private float shippingCost;
+    private float productCost;
+    private float subtotal;
+    private float tax;
+    private float total;
+
+    private int deliveryDays;
+    private  Date deliveryDate;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderDetail> orderDetails = new HashSet<>();
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("updatedTime ASC")
+    private List<OrderTrack> orderTracks = new ArrayList<>();
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public float getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(float shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    public float getProductCost() {
+        return productCost;
+    }
+
+    public void setProductCost(float productCost) {
+        this.productCost = productCost;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public float getTax() {
+        return tax;
+    }
+
+    public void setTax(float tax) {
+        this.tax = tax;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public int getDeliveryDays() {
+        return deliveryDays;
+    }
+
+    public void setDeliveryDays(int deliveryDays) {
+        this.deliveryDays = deliveryDays;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Set<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(Set<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public void copyAddressFromCustomer() {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public List<OrderTrack> getOrderTracks() {
+        return orderTracks;
+    }
+
+    public void setOrderTracks(List<OrderTrack> orderTracks) {
+        this.orderTracks = orderTracks;
+    }
+
 
 
 }
